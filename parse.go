1692d0cbda04b1e46ad7d25fbd72a4fb320f4085
Carl Johnson <carl@carlsensei.com>
2012-10-15 00:34:33 -1000

    Truly awful basic parser for LichData.

--- parse.go
+++ parse.go
@@ -1,5 +1,40 @@
 package lich
 
+import (
+	"errors" //Why would anyone want to import more errors into their code???
+	"strconv"
+)
+
+var UnparseableError = errors.New("Couldn't parse string.")
+
 func Parse(s string) (Element, error) {
-	return Data(""), nil
+	if len(s) < 1 {
+		return nil, UnparseableError
+	}
+	r := rune(s[0])
+
+	if !isdigit(r) {
+		return nil, UnparseableError
+	}
+
+	start, stop := 0, 1
+	for isdigit(rune(s[stop])) {
+	}
+
+	size, err := strconv.Atoi(s[start:stop])
+
+	if err != nil {
+		//I don't think this is reachable.
+		return nil, UnparseableError
+	}
+
+	if rune(s[stop]) != '<' || rune(s[stop+size+1]) != '>' {
+		return nil, UnparseableError
+	}
+
+	return Data(s[stop+1 : stop+size+1]), nil
+}
+
+func isdigit(r rune) bool {
+	return r >= '0' && r <= '9'
 }
